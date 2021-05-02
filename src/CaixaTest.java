004a6a4b68f3a301487026c2db204d00554387b9
Sabrinna <sabrinna13.si@gmail.com>
2021-05-02 18:18:29 -0300

    Adicionei a classe teste

---
+++ src/CaixaTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaixaTest {
+
+    @Test
+    void deposito() {
+    }
+
+    @Test
+    void saque() {
+    }
+
+    @Test
+    void saldo() {
+    }
+}
