49371819c44cde541f0d7f786fb3dcd95795a3da
Sabrinna <sabrinna13.si@gmail.com>
2021-05-02 18:11:37 -0300

    Adicionei os métodos depósito, saque e saldo

--- src/Caixa.java
+++ src/Caixa.java
@@ -4,4 +4,13 @@ public class Caixa {
     public Caixa(double saldo) {
         this.saldo = saldo;
     }
+    public void deposito(double valor){
+        this.saldo+=valor;
+    }
+    public void saque(double valor){
+        this.saldo-=valor;
+    }
+    public double saldo(){
+        return this.saldo;
+    }
 }
